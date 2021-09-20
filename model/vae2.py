cec60993f7f31572b2a9a3af494d7a319dd247df
fhzh123 <fhzh@naver.com>
2021-09-20 21:57:11 +0900

    fix

--- model/vae2.py
+++ model/vae2.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from transformers import EncoderDecoderConfig, EncoderDecoderModel
 
 class SentenceVAE(nn.Module):
-    def __init__(self, encoder_config, decoder_config, d_latent, max_len):
+    def __init__(self, encoder_config, decoder_config, d_latent, device):
         super().__init__()
 
         """
@@ -15,7 +15,7 @@ class SentenceVAE(nn.Module):
             encoder_config (dictionary): encoder transformer's configuration
             encoder_config (dictionary): decoder transformer's configuration
             d_latent (int): latent dimension size
-            max_len (int): max length of input sequence
+            device (torch.device): 
         Returns:
             log_prob (torch.Tensor): log probability of each word 
             mean (torch.Tensor): mean of latent vector
@@ -24,11 +24,13 @@ class SentenceVAE(nn.Module):
         """
 
         self.d_latent = d_latent
-        self.max_len = max_len
+        self.device = device
 
         self.d_hidden = encoder_config.hidden_size
 
         EncoderDecoderConfig = EncoderDecoderConfig.from_encoder_decoder_configs(encoder_config, decoder_config)
+        self.vocab_size = encoder_config.vocab_size
+
         self.seq2seq_bert = EncoderDecoderModel(config=EncoderDecoderConfig)
         self.seq2seq_bert_encoder = self.seq2seq_bert.encoder
         self.seq2seq_bert_decoder = self.seq2seq_bert.decoder
@@ -36,6 +38,22 @@ class SentenceVAE(nn.Module):
         self.hidden2mean = nn.Linear(self.d_hidden, self.d_latent)
         self.hidden2logv = nn.Linear(self.d_hidden, self.d_latent)
         self.latent2hidden = nn.Linear(self.d_latent, self.d_hidden)
+
+    def reparameterization(self, enc_out):
+        """
+        Re-parameterization trick
+
+        Args:
+            enc_out (): 
+        """
+        mu = self.hidden2mean(enc_out.last_hidden_state)
+        logv = self.hidden2logv(enc_out.last_hidden_state)
+        std = torch.exp(0.5 * logv)
+
+        z = torch.randn(enc_out.last_hidden_state.size(), device=self.device)
+        z = z * std + mean
+
+        return mu, logv, z
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         """
@@ -50,32 +68,27 @@ class SentenceVAE(nn.Module):
             log_var (torch.Tensor): log variance of latent vector
             z (torch.Tensor): sampled latent vector
         """
-        batch_size = input_sequence.size(0)
-        encoder_input_sequence = input_sequence.clone()
-        decoder_input_sequence = input_sequence.clone()
+        batch_size = input_ids.size(0)
+        encoder_input_ids = input_ids.clone()
+        decoder_input_ids = input_ids.clone()
 
         # Encoder forward pass
-        enc_out = self.seq2seq_bert_encoder(input_ids=input_ids,
+        enc_out = self.seq2seq_bert_encoder(input_ids=encoder_input_ids,
                                             attention_mask=attention_mask,
                                             token_type_ids=token_type_ids)
 
-        # Flatten
-
         # Re-parameterization
-        mu = self.hidden2mean(enc_out.last_hidden_state)
-        logv = self.hidden2logv(enc_out.last_hidden_state)
-        std = torch.exp(0.5 * logv)
-
-        z = torch.randn([batch_size, self.d_latent])
-        z = z * std + mean
+        mu, logv, z = self.reparameterization(enc_out)
 
         # Decoder input processing
         hidden = self.latent2hidden(z)
-        input_sequence = self.embedding_dropout(input_sequence)
+        decoder_input_ids = self.embedding_dropout(decoder_input_ids)
 
         # Decoder forward pass
-        outputs = self.seq2seq_bert_decoder(input_ids=input_ids,
+        outputs = self.seq2seq_bert_decoder(input_ids=decoder_input_ids,
                                             attention_mask=attention_mask,
                                             token_type_ids=token_type_ids,
                                             encoder_hidden_states=enc_out.last_hidden_state,
                                             encoder_attention_mask=attention_mask)
+
+        return outputs, mu, logv, z
