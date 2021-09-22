0064613daebe23b60224736c981a701d26e352ba
fhzh123 <fhzh@naver.com>
2021-09-23 00:11:10 +0900

    augmentation coding init

--- task/augmentation.py
+++ task/augmentation.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+# Import Custom Modules
+from model.dataset import CustomDataset
 
 def augmenting(args):
 
@@ -24,5 +26,17 @@ def augmenting(args):
         train_token_type_ids = data_['train']['token_type_ids']
         train_attention_mask = data_['train']['attention_mask']
         train_comment_indices = data_['train_comment_indices']
-        train_label = data_['train']['train_label']
+        train_label = data_['train']['label']
         del data_
+
+    # 2) Dataloader setting
+    dataset_dict = {
+        'train': CustomDataset(train_comment_indices, train_label, 
+                               min_len=args.min_len, max_len=args.max_len)
+    }
+    dataloader_dict = {
+        'train': DataLoader(dataset_dict['train'], collate_fn=PadCollate(), drop_last=False,
+                            batch_size=args.batch_size, shuffle=True, pin_memory=True,
+                            num_workers=args.num_workers)
+    }
+    print(f"Total number of trainingsets  iterations - {len(dataset_dict['train'])}, {len(dataloader_dict['train'])}")
